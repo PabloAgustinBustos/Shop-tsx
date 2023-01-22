85dc9c0a296a4f8d9f4704e5bf3d5b1452d874e0
Pablo Bustos <pablobustos609@gmail.com>
2023-01-22 20:43:00 -0300

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import {Routes, Route} from "react-router-dom"
+import {Container} from "react-bootstrap"
+
+import Home from "./pages/Home"
+import Store from "./pages/Store"
+import About from "./pages/About"
 
 function App() {
-  
-
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/store" element={<Store/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Container>
   )
 }
 
